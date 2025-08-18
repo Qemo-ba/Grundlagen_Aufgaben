22364e85929fdb35137e559d027962e5f6199511
Qemal Badalli <qemal>
2025-08-18 09:46:45 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            int a = 2;
+            int b = 3;
+            int summe = myMath.Add(a, b);
+            Console.WriteLine($"summe von {a} und {b} ist {summe}");
+        }
+    }
+}
